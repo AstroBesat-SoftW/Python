be59d5f9367a75ce214cff2d22e1ed24b963da32
AstroBesat <128177174+AstroBesat-SoftW@users.noreply.github.com>
2023-03-17 22:24:02 +0300

    Update hafta_1.py

--- hafta_1.py
+++ hafta_1.py
@@ -3,7 +3,7 @@
 print('merhaba nasılsın?')
 print('ismin ne?')
 isim = input()  # kullanıcıdan girdi alıyorum
-print('memnun oldum' + ' '+isim)
+print('memnun oldum ' +isim)
 print('isminde bu kadar harf var')
 print(len(isim))  #len lenght anlamında uzunlık için
 print('kaç yaşındasın?')
@@ -11,3 +11,16 @@ isim = input()  #girdi alıyorum kullanıcıdan
 print('sen seneye ' + str(int(isim) +1) + 'yaşında olucan')  #int tipi
 
 
+-------- bu alt kısmı sil ----------
+
+Çıktı:
+
+Merhaba nasılsın?
+İsmin ne?
+>>Cansu (bunu sen girdin varsay)
+Memnun oldum Cansu
+İsminde bu kadar harf var 
+5
+kaç yaşındasın?
+>>20 (bunu sen girdin varsay)
+sen seneye 21 yaşında olucan
