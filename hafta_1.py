000d4dd85c4cf3cfeb656f0a96f5556afb4d87c8
AstroBesat <128177174+AstroBesat-SoftW@users.noreply.github.com>
2023-03-17 22:19:26 +0300

    Add files via upload

---
+++ hafta_1.py
@@ -0,0 +1,13 @@
+# ilk programım
+
+print('merhaba nasılsın?')
+print('ismin ne?')
+isim = input()  # kullanıcıdan girdi alıyorum
+print('memnun oldum' + ' '+isim)
+print('isminde bu kadar harf var')
+print(len(isim))  #len lenght anlamında uzunlık için
+print('kaç yaşındasın?')
+isim = input()  #girdi alıyorum kullanıcıdan
+print('sen seneye ' + str(int(isim) +1) + 'yaşında olucan')  #int tipi
+
+
