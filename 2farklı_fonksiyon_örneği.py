d6a14d00d36f64d93b9472c1a540606cc6a5bcb9
AstroBesat <128177174+AstroBesat-SoftW@users.noreply.github.com>
2023-06-24 22:05:00 +0300

    Add files via upload

---
+++ 2farklı_fonksiyon_örneği.py
@@ -0,0 +1,35 @@
+#parametreli yapıcaksak eğer
+
+def param(name):
+    print('merhaba ' +name)
+
+param('besat')
+param('cansu')
+param('naberyaaa')
+
+
+# farklı deneyelim
+print('-'*50)
+def hesap(islem):
+    sonuc= islem+islem 
+    print(sonuc)
+
+
+islem =10
+hesap(islem)
+
+#buda 2 diğerinden ayı üsteki farklı altaki farklı
+
+while (1 == 1):
+    def ing(hello):
+        if (hello == 'hello'):
+            print('merhaba')
+
+        elif (hello == 'brother'):
+            print('erkek kardeş')
+
+
+    print('ingilizce bir şey yazın')
+    isim= input()
+
+    ing(isim)
