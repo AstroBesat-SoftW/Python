7148194f5776dcfc54904a5e16bf06aaf437442a
AstroBesat <128177174+AstroBesat-SoftW@users.noreply.github.com>
2023-06-25 23:01:08 +0300

    Add files via upload

---
+++ şifre oluşturup asal sayıla çözme.py
@@ -0,0 +1,14 @@
+#şifre oluşturup onu asal sayı ile çözme
+
+print('merhaba asal sayılı kırmak istediğin şifreyi gir: ')
+
+
+sifre = input() #sbegsuasdatsssssssssssssssssssssss bunu girdik mesela
+#cevap: örnek ben 30 a kadar asal sayıları yapıcam
+print('-'*50)
+print('girilen yazı: ' + sifre)
+print('-'*50)
+print('Çözülmüş hali:')
+print(sifre[1]+sifre[2]+sifre[4]+sifre[6]+sifre[10]+sifre[12]+sifre[16]+sifre[18]+sifre[22]+sifre[28])
+#1 den başladım 1 normalde asal sayı değil ama indis 0 dan başlar o yüzden
+
