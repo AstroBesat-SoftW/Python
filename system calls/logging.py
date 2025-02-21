bdd91fe99739f85e0efbd35293bc1592d89fcc47
AstroBesat <128177174+AstroBesat-SoftW@users.noreply.github.com>
2025-02-21 22:41:46 +0300

    Delete system calls directory

--- system calls/logging.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(filename="detailed_system_log.txt", level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-def log_event(event):
-    logging.info(event)
-
-log_event("Sistem Çağrısı: read() - Başarılı")
-log_event("Sistem Çağrısı: write() - Başarılı")
