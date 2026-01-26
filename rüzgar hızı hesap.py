ad27a06e26462cc32d29ec9bc5f247530df101ce
Besat Ç. <128177174+AstroBesat-SoftW@users.noreply.github.com>
2026-01-26 21:48:30 +0300

    Clarify purpose of wind speed calculation code
    Added comment to clarify the purpose of the code.

--- rüzgar hızı hesap.py
+++ rüzgar hızı hesap.py
@@ -1,7 +1,8 @@
-#rüzgarın hızını hesaplama...
+#rüzgarın hızını hesaplama... bu o an ölçülen deneme yanılma verileri
 while  1 == 1:
     t = input('Pervanenin dönüş hızını gir \n>')
 
     sonuc = 96/int(t)
 
     print(sonuc)
+
