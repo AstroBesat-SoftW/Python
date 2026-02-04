98fc0b1dd49c3e62321833dd87f990c42bdea254
Besat Ç. <128177174+AstroBesat-SoftW@users.noreply.github.com>
2026-02-04 12:56:46 +0300

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,14 @@
+# if else yapısı basit ders 1
+
+sayi_1 = 1
+sayi_2 = 2
+print(sayi_1 + sayi_2)
+ # hata verir mi verise nerede veri?
+
+if sayi_1 == 1:
+    print("sayı 1")
+    
+else:
+    print("sayı 1 değil") 
+    
+# hata verir mi? vermzse neden?
