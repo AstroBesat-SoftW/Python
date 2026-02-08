f111e1dff74fe96b7d79bdbe275455b1c0d44afd
Besat Ç. <128177174+AstroBesat-SoftW@users.noreply.github.com>
2026-02-08 17:07:36 +0300

    Create toplama_ders_5.py

---
+++ toplama_ders_5.py
@@ -0,0 +1,14 @@
+# kullanıcıdan iki girdi alarak toplama işlemi yapıyoruz
+
+a = input()
+b = input()
+
+c = a + b
+
+print(c)
+
+# girilen a ve b değerlerini toplar
+# ekrana yazar
+# bir sonraki işlem fonksiyon ile bunu yapmak
+
+# ders 5
